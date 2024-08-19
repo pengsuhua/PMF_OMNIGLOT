2b3a5fade013035fb68131b2c5382bb044c712e2
pengsuhua <116246948+pengsuhua@users.noreply.github.com>
2024-08-19 15:09:35 +0800

    Delete PMF(and add prior for wmm) directory

--- PMF(and add prior for wmm)/modeler/@Modeler/loadlib.m
+++
@@ -1,9 +0,0 @@
-%
-% Load library from a file, 
-% as specified in the function defaultps
-%
-function lib = loadlib()
-    ps = defaultps;
-    load(ps.libname,'lib');
-    assert(isa(lib,'Library'));
-end
